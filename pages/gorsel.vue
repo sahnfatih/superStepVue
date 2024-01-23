f1c0e9283ff8cfedb8fcfdf078833559ff9f82b4
sahnfatih <sahnfatih49@gmail.com>
2024-01-24 00:19:10 +0300

    urun gorseli eklendi

---
+++ pages/gorsel.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <NavBar/>
+        <Urun/>
+        <Footer2Vue />
+        
+    </div>
+    
+</template>
+
+
+<script>
+import Footer2Vue from '~/components/Footer2.vue'
+import NavBar from '~/components/NavBar.vue';
+import Urun from '~/components/Urun.vue';
+
+export default {
+    components: { Urun ,NavBar, Footer2Vue}
+}
+</script>
